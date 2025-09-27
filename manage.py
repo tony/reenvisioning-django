61b21756df1fd23356411ca67975e7f18553c480
Tony Narlock <tony@git-pull.com>
2025-09-27 08:54:06 -0500

    manage(fix[settings]): Update settings module path
    why: ModuleNotFoundError when running manage.py commands
    what:
    - Change DJANGO_SETTINGS_MODULE from 'envision.settings'
      to 'envision.settings.base'
    
    refs: Matches actual project structure

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main() -> None:
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "envision.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "envision.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
